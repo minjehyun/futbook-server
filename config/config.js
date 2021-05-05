9ad8257e3c9f098a8bddf73bf65a3014a14e7f76
민제현 <alswpgus0@gmail.com>
2021-05-05 15:50:54 +0900

    feature_deployEnvironment_#001:Modified deploy environment

--- config/config.js
+++ config/config.js
@@ -14,6 +14,7 @@ const config = {
     password: process.env.DATABASE_PASSWORD,
     database: "futbook",
     host: process.env.DATABASE_HOST,
+    port: process.env.DATABASE_PORT,
     dialect: "mysql",
   },
 };
